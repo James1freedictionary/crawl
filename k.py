0a776c01c4385deffa36db2ca30154eb3516dc31
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2022-04-16 20:41:00 +0700

    Update k.py

--- k.py
+++ k.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 import time
 l= Flask(__name__)
-dir = "\n".join([f for f in os.scandir(".")])
+dir = "\n".join([f.name for f in os.scandir(".")])
 @l.route("/")
 def index():
     return dir
