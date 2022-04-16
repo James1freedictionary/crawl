08f5bde08670a06273315f6f25321cc614dd34e1
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2022-04-16 21:04:34 +0700

    Update k.py

--- k.py
+++ k.py
@@ -4,7 +4,7 @@ import time
 l= Flask(__name__)
 dir = [f.name for f in os.scandir(".")]
 b = [os.path.getsize(f) for f in [dir[f] for f in range(len(dir))]]
-z = ";".join([x + " " + y for x,y in zip(dir,b)])
+z = ";".join([x + " " + str(y) for x,y in zip(dir,b)])
 @l.route("/")
 def index():
     return z
