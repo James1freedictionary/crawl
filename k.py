03f31048b1099c15060d207bf596b053b358be5b
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2022-04-16 21:03:01 +0700

    Update k.py

--- k.py
+++ k.py
@@ -2,7 +2,9 @@ from flask import Flask
 import os
 import time
 l= Flask(__name__)
-dir = "\n".join([f.name for f in os.scandir(".")])
+dir = [f.name for f in os.scandir(".")]
+b = [os.path.getsize(f) for f in [dir[f] for f in range(len(dir))]]
+z = ";".join([x + " " + y for x,y in zip(dir,b)])
 @l.route("/")
 def index():
-    return dir + "this\nme"
+    return z
