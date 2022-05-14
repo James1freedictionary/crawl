765ad07aa468c7047986167e0128aeccf156069f
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2022-05-14 23:34:50 +0700

    Update f_app.py

--- f_app.py
+++ f_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 dir = [f.name for f in os.scandir(".")]
 b = [os.path.getsize(f) for f in [dir[f] for f in range(len(dir))]]
 z = ";".join([x + " " + str(y) for x,y in zip(dir,b)])
-@l.route("/")
+@app.route("/")
 def index():
     #subprocess.run(["python", "crawl.py"], shell=True)
     return z
